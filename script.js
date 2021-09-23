54abd4af69ee234fe03dba3ad3e5bee8d4e99411
Chris <chris.feight@gmail.com>
2021-09-23 01:31:27 -0700

    pwgenerator js first draft

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var choices = "";
+var passwordText = "";
 
 //constant identifications
 
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_"];
+const upperCase = ['A' + 'B' + 'C' + 'D' + 'E' + 'F' + 'G' + 'H' + 'I' + 'J' + 'K' + 'L' + 'M' + 'N' + 'O' + 'P' + 'Q' + 'R' + 'S' + 'T' + 'U' + 'V' + 'W' + 'X' + 'Y' + 'Z' ];
+const lowerCase = ["a" + "b" + "c" + "d" + "e" + "f" + "g" + "h" + "i" + "j" + "k" + "l" + "m" + "n" + "o" + "p" + "q" + "r" + "s" + "t" + "u" + "v" + "w" + "x" + "y" + "z" ];
+const numbers = ['0' + '1' + '2' + '3' + '4' + '5' + '6' + '7' + '8' + '9' ];
+const specChar = ["!" + "@" + "#" + "$" + "%" + "^" + "&" + "*" + "(" + ")" + "-" + "_ "];
+
+//+ symbol removes any space between ""
 
 // Write password to the #password input
-function writePassword() {
+function generatePassword() {
   var passwordLength = prompt("Choose the length of your password (between 8 and 128 characters");
 
     if ( 
       passwordLength < 8
       ||
       passwordLength > 128
-       ) {
+//      ||
+//      Number.isInteger(passwordLength) == false
+
+// How do I provide a negative response to the integer where it will not accept a letter after repeated password generations?
+       ) 
+    {
         alert ("You MUST pick a length between 8 and 128 characters");
         return;
     }
@@ -68,23 +76,37 @@ function writePassword() {
           ) { 
           alert ("You MUST choose Y or N");
         return;
-      }
-  
-        if (hasUpper == "Y") {
+      } 
 
+      if (hasUpper ==  "N" && hasLower == "N" && hasNumbers == "N" && hasSpecChar == "N") {
+        alert("You must choose at least one character type"); 
+        return;
+      }
+      
+        if (hasUpper == "Y") {
+          choices = choices.concat(upperCase);
         }
-  
+        if (hasLower == "Y") {
+          choices = choices.concat(lowerCase);
+        }
+        if (hasNumbers == "Y") {
+          choices = choices.concat(numbers);
+        }
+        if (hasSpecChar == "Y") {
+          choices = choices.concat(specChar);
+        }
+      
+        for(let i = 0 ; i < passwordLength ; i++) {
+          var temp1 = Math.floor(Math.random() * choices.length);
+          passwordText += choices[temp1];   
+        }
+        document.getElementById("password").innerHTML=passwordText;
+        passwordText = "";
+        choices = "";
 
 }
-  
-
-//var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
   
 
 
@@ -112,3 +134,7 @@ generateBtn.addEventListener("click", writePassword);
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
 //TODO: take our return array and use array methods to convert to a string --> have a var for this and return string
+
+//var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
